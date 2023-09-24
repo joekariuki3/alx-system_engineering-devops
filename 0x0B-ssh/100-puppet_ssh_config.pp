dac5a87c810e0c0cd1fc658b37164661aaec2ebb
joekariuki3 <joekariuki3@gmail.com>
2023-09-24 09:32:53 +0300

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,7 @@
 # using puppet to change ssh config file
 file {'/etc/ssh/ssh_config':
   ensure  => present,
-  content => "StrictHostKeyChecking ask\nIdentityFile ~/.ssh/school/nPasswordAuthentication no/n"
+  content => "StrictHostKeyChecking ask
+	  IdentityFile ~/.ssh/school
+	  PasswordAuthentication no"
 }
