792d1672811ec10b0c080c39469c015e111beca1
joekariuki3 <joekariuki3@gmail.com>
2023-09-27 21:30:13 +0300

    cnfigure a server using puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,19 @@
 # manifest to install nginx, should perform redirect 301
+
 package { 'nginx':
   ensure => 'installed',
 }
 
-file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server',
-  line   => 'rewrite ^/redirect_me google.com parmanent'
-}
 
 file { '/var/www/html/index.html':
+  ensure  => 'file',
   content => 'Hello World!',
+  mode    => '0644',
+  require => Package['nginx'],
+}
+
+exec { 'append_redirect_me':
+  command => "/usr/bin/sed -i '/i^}$/i \\\n\tlocation \\/redirect_me {return 301 google.com;}' /etc/nginx/sites-available/default",
 }
 
 service { 'nginx':
