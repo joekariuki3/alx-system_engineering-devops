ad2cd4bbc9eb50e506430c7a567d2edd268635ce
joekariuki3 <joekariuki3@gmail.com>
2023-10-02 20:03:19 +0300

    removed unwanted repository

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# script that takes url, send a request and display size of the body curl s silent I header L location(redirection) grep i case
-curl -sIL "$1" | grep -i 'Content-Length:' | awk '{print $2}'
