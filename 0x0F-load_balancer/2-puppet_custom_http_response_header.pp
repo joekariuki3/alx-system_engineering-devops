5e7c9cb65a869847cd8c0301c883fd1fe950c71e
joekariuki3 <joekariuki3@gmail.com>
2023-10-03 15:25:34 +0300

    set header automate using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,21 @@
 # manifest to install nginx, set a custom header
 
+# ensure package nginx is intalled in the system
 package { 'nginx':
   ensure => 'installed',
 }
 
-augeas { 'nginx_add_custom_header':
-  context => '/etc/nginx/sites-available/default',
-  changes => ['set add_header[last()+1] X-Served-By $hostname'],
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+# get ststem hostname using facter variable
+$system_hostname = $::hostname
+
+# make sure line for custom header is in the default file
+file_line{ 'add_custom_header':
+  path  => '/etc/nginx/sites-available/default',
+  line  => "\tadd_header X-Served-By ${system_hostname}",
+  after => 'root /var/www/html;',
 }
 
+# ensure nginx service is running and it is enabled
 service { 'nginx':
   ensure  => running,
   enable  => true,
