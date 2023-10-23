5176190fb0445714dad5675bc427fb9fe00c1a95
joekariuki3 <joekariuki3@gmail.com>
2023-10-23 21:47:34 +0300

    fix import format and result format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     import requests
     import sys
 
-
     if len(sys.argv) > 1:
         user_id = sys.argv[1]
         if user_id.isdigit():
@@ -15,11 +14,11 @@ if __name__ == '__main__':
             url_user = f'https://jsonplaceholder.typicode.com/users/{user_id}'
             response_user = requests.get(url_user)
             user_data = response_user.json()
-            user_name = user_data.get('name')
+            user = user_data.get('name')
             url = 'https://jsonplaceholder.typicode.com/todos/'
             response = requests.get(url)
             all_to_do = response.json()
-            ad_length = len(all_to_do)
+            ad_len = len(all_to_do)
             finished_to_do = 0
             done_to_do = []
             for to_do in all_to_do:
@@ -28,6 +27,6 @@ if __name__ == '__main__':
                         finished_to_do += 1
                         done_to_do.append(to_do.get('title'))
             d_len = len(done_to_do)
-            print(f'Employee {user_name} is done with tasks({d_len}/{ad_length}):')
+            print(f'Employee {user} is done with tasks({d_len}/{ad_len}):')
             for to_do in done_to_do:
                 print(f"\t {to_do}")
